05b11b9b58a3cb8f0793b61b8ba255c9d50aa97b
Rafał Wróblewski <rwroblewski@advisor247.pl>
2020-02-10 08:29:36 +0100

    new order changes --still not working

--- VirtualServersApp/Shared/Models/Order.cs
+++ VirtualServersApp/Shared/Models/Order.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace VirtualServersApp.Shared.Models {
 
     public class Order {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Product field is required!")]
         public int ProductId { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool Completed { get; set; }
-
-        public string StringProductId { get; set; }
     }
 }
